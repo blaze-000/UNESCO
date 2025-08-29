575fb2e5635fb0293b634bac84fcc65b19e543d5
Asmit Sah <asmitsah36@gmail.com>
2025-08-29 13:53:47 +0545

    Analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Header from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
