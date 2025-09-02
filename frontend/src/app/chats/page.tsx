487561a00629b54136dc06fe006b1c6a06b407c7
Asmit Sah <asmitsah36@gmail.com>
2025-09-02 22:31:26 +0545

    Chat Box

--- frontend/src/app/chats/page.tsx
+++ frontend/src/app/chats/page.tsx
@@ -25,8 +25,9 @@ type Message = {
 const BACKEND_URL = "https://rishi-kesh-00-luma.hf.space/";
 
 const EXAMPLE_PROMPTS = [
-  "Colgate Optic White Renewal is a high-performance whitening toothpaste designed to remove up to 15 years of stains with its 5% hydrogen peroxide formula. It targets deep-set discoloration from coffee, tea, and wine while remaining gentle on enamel. With a refreshing mint flavor and fluoride protection, it not only brightens your smile but also strengthens teeth and fights cavities.",
   "Infused with Vitamin C and turmeric, Mamaearth’s Vitamin C Face Wash is crafted to illuminate dull skin and fight free radical damage. Its antioxidant-rich formula gently cleanses while promoting an even skin tone and natural radiance. Suitable for all skin types, it’s free from sulfates and parabens, making it a safe choice for daily use.",
+  "Colgate Optic White Renewal is a high-performance whitening toothpaste designed to remove up to 15 years of stains with its 5% hydrogen peroxide formula. It targets deep-set discoloration from coffee, tea, and wine while remaining gentle on enamel.",
+  
 ];
 
 const InputComponent = React.memo(
